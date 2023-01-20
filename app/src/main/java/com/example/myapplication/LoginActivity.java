c8bd1b1064a66de86f441d6cf2f8cd301ec3c745
LeeHyunJin <myfamily11141@gmail.com>
2023-01-20 15:54:07 +0900

    화면 넘어가기 제작

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -3,12 +3,24 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        a= (Button)findViewById(R.id.start_btn);
+
+        a.setOnClickListener(new View.OnClickListener()){
+            public void onClick(View v){
+                Intent myIntent = new Intent(LoginActivity.this, activi)
+            }
+        }
     }
 }
