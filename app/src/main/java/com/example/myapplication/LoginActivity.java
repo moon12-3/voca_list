c6094242b7d40e1cf123b577af92be33b313cbb5
LeeHyunJin <myfamily11141@gmail.com>
2023-02-08 23:59:36 +0900

    화면 넘어가기

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -12,7 +12,7 @@ public class LoginActivity extends AppCompatActivity {
     Button a;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //공부 추가
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
